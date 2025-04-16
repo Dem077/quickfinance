8fd5279a7e84e26c639058e132baa40409c87486
Mohamed Linaf <linaf7@gmail.com>
2025-04-16 15:33:53 +0500

    Feature - update MailStart command signature and description to clarify its purpose and functionality

--- app/Console/Commands/MailStart.php
+++ app/Console/Commands/MailStart.php
@@ -11,20 +11,26 @@ class MailStart extends Command
      *
      * @var string
      */
-    protected $signature = 'app:mail-start';
+    protected $signature = 'mailstart';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Start the queue listener in the background using nohup';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        // Run the nohup command to start the queue listener in the background
+        $command = 'nohup php artisan queue:listen &';
+        exec($command);
+
+        $this->info('Queue listener started in the background.');
+
+        return Command::SUCCESS;
     }
 }
